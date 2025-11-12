39661559733aa49684e911eba9c4a2901fe39386
Vinícius Prata <108702285+SirArucard@users.noreply.github.com>
2025-11-12 15:35:10 -0300

    funcao imprimeTitulo criada e começo da recebeNum

--- LMC.java
+++ LMC.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class LMC {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        imprimeTitulo();
         }
+    public static void imprimeTitulo(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("===============================================");
+        System.out.println("Bem vindo ao cálculo de MMC!");
+        System.out.println("Aqui você poderá calcular o MMC entre n números");
+        System.out.println("Digite 1 para continuar: ");
+        System.out.println("===============================================");
+        int opcao = entrada.nextInt();
+        while (opcao != 1) {
+            System.out.println("Digita 1 ai faz favô.");
+            opcao = entrada.nextInt();
+        }
+        System.out.println("\n\n Show! Vamos começar?");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+    }
+    public static int[] recebeNum(int[] num){
+        System.out.println("Me fala quais numeros voce quer tirar MMC:(Digite numero a numero e aperte enter) (Digite 0 ao concluir)");
+        
+    }
     }
