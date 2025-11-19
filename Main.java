603b5cb985d05458148ee81dad1db0d6c6785bcf
Vinícius Prata <108702285+SirArucard@users.noreply.github.com>
2025-11-19 14:12:13 -0300

    Refine comments for better understanding
    Updated comments for clarity and detail throughout the Main.java file.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
     // Classe principal do código
+    // De forma resumida: Chamamos a função que imprime o titulo, depois declaramos um array que recebe os numeros de retorno da função recebNum
+    // declaramos uma variavel que recebe a função que executa o mmc e retorna o resultado
 public class Main {
     public static void main(String[] args) {
         imprimeTitulo();
@@ -8,7 +10,7 @@ public class Main {
         System.out.println("O MMC dos números informados é: " + resultadoMmc);
 
         }
-        // Classe que imprime um menuzinho na tela
+        // Classe que imprime um menuzinho na tela, sem nenhum segredo
     public static void imprimeTitulo(){
         
         System.out.println("===============================================");
@@ -18,7 +20,9 @@ public class Main {
         System.out.println("===============================================");
         System.out.println("===============================================");
     }
-        // Classe que pede a quantidade de numeros que o usuário quer fazer o MMC, calculando o tamanho do array 
+        // Classe que pede a quantidade de numeros que o usuário quer fazer o MMC, calculando o tamanho do array
+        // Ela pede ao usuário a quantidade de numeros e guarda esse valor numa variável que será lida como quantos numeros nosso array terá
+        // Depois disso criamos um for que passa e armazena cada valor que será usado para o MMC em uma posição diferente (Os indices)
     public static int[] recebeNum(){
         Scanner entrada = new Scanner(System.in);
         System.out.println("De quantos numeros você quer tirar o MMC?");
@@ -34,6 +38,10 @@ public class Main {
         
     }
         // Aqui executamos a raiz da questão, testando valor por valor do array com numeros primos
+        // Declaramos duas variáveis, uma pra guardar o valor total do MMC e uma que será o divisor primo
+        // Criamos um while que tem como parametro a NEGAÇÃO (!) da função SaoUm, que será explicada lá na linha dela
+        // Com essa função booleana, conseguimos rodar esse while, numero a numero do nosso array e realizar a divisão com teste de resto (usando %) para calcular 
+        // Fora do while chamamos um if para chamar a função que imprime na tela o macetinho como pedido, ex.: 10 10 10 10 | 2
     public static int executaMDC(int[] numeros){
         int totalMmc = 1;
         int divPrimo = 2;
@@ -55,7 +63,7 @@ public class Main {
             }
             return totalMmc;
         }
-        // Classe que serve apenas pra ser usada no while do código principal e parar de rodar quando todos numeros são 1 no array
+        // Classe que serve apenas pra ser usada no while da função executaMDC e parar de rodar quando todos numeros são 1 no array
     public static boolean SaoUm(int[] numeros) {
     for (int i = 0; i < numeros.length; i++) {
         if (numeros[i] != 1) {
@@ -64,7 +72,9 @@ public class Main {
     }
     return true;
     }
-        // Impressão na tela bonitinha do macete, pelo menos é assim que o Marcos chama
+        // Impressão na tela bonitinha do macete, foi assim que cê chamou professor, acho né? kkk
+        // Sem muito segredo ele simplesmente passa pelo array imprimindo cada numero e fora do for ele puxa a | + o divisor pra ficar na ordem
+        // Lembra um pouco o que fizemos pra imprimir os letrão, de uma forma ou outra 
     public static void imprimeMacete(int[] numeros, int divisor) {
         for (int i = 0; i < numeros.length; i++) {
             System.out.print(numeros[i] + " ");
